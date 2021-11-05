477bf8a2359211b1fd2a811b8ccb4eebf6c0ceb0
0xIvan <0xivanivan@gmail.com>
2021-11-05 17:17:38 +0100

    Initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Ilisarniq", "sans-serif"],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
