f6b81caae90b3715a090b0e0ba415c478c055a72
0xIvan <0xivanivan@gmail.com>
2021-11-10 13:23:36 +0100

    Add prop to text input

--- src/components/TextInput/index.tsx
+++ src/components/TextInput/index.tsx
@@ -1,17 +1,23 @@
+import clsx from "clsx";
+
 interface Props {
   placeholder: string;
   onChange: (input: string) => void;
   value: string;
+  alignRight?: boolean;
 }
 
 export const TextInput: React.FC<Props> = (props) => {
-  const { placeholder, onChange, value } = props;
+  const { placeholder, onChange, value, alignRight } = props;
 
   return (
     <input
       type="text"
       placeholder={placeholder}
-      className="relative w-full px-2 py-1 text-sm text-gray-600 placeholder-gray-400 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:ring"
+      className={clsx(
+        alignRight && "text-right",
+        "relative w-full px-2 py-1 text-sm text-gray-600 placeholder-gray-400 bg-white border border-gray-400 rounded outline-none focus:outline-none focus:ring"
+      )}
       onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
         onChange(event.target.value)
       }
